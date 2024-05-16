07bca09ebdba0c1fb6d484eafc3ba145994b37bb
Lance Harvey Bazar <lnchrv@gmail.com>
2024-05-16 17:03:19 +0800

    log-in

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [MatButtonModule,MatInputModule,MatCardModule,CommonModule,FormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+  user = "";
+  pass = "";
+
+  authorize = "null";
+
+
+  confirm(){
+    if(this.user === "Admin" && this.pass === "Admin"){
+      this.authorize = "true";
+      setTimeout(()=>{this.authorize = "null"},2000);
+    }
+    else
+    {
+      this.authorize = "false";
+      setTimeout(()=>{this.authorize = "null"},2000);
+    }
+    
+  }
+
+}
